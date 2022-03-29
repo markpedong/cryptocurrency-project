090025859aa3d01d8b2742051e966f0e54cd4a74
markpedong <mark.pedong01@gmail.com>
2022-03-29 20:13:16 +0800

    Added Styles in Header

--- src/Components/Pages/TopHeader.tsx
+++ src/Components/Pages/TopHeader.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Col, Container, Row } from "react-bootstrap";
 import { HeaderTitle, HeaderDesc } from "../../Styles/MainHeaderStyle";
+import { FaMoon } from "react-icons/fa";
 import "../../Styles/header.scss";
 
 type Props = {};
@@ -8,10 +9,30 @@ type Props = {};
 const TopHeader = ({}: Props) => {
   return (
     <Container fluid>
-      <Row>
-        <Col className="header_container">
+      <Row className="header_row">
+        <Col xs={"auto"} className="header_container">
           <HeaderTitle>Cryptos:</HeaderTitle>
           <HeaderDesc>18,542</HeaderDesc>
+        </Col>
+        <Col xs={"auto"} className="header_container">
+          <HeaderTitle>Exchanges:</HeaderTitle>
+          <HeaderDesc>484</HeaderDesc>
+        </Col>
+        <Col xs={"auto"} className="header_container">
+          <HeaderTitle>Market Cap:</HeaderTitle>
+          <HeaderDesc>₱112,319,194,629,451</HeaderDesc>
+        </Col>
+        <Col xs={"auto"} className="header_container">
+          <HeaderTitle>24h Vol:</HeaderTitle>
+          <HeaderDesc>₱6,289,330,421,958</HeaderDesc>
+        </Col>
+        <Col xs={"auto"} className="header_container">
+          <HeaderTitle>Dominance:</HeaderTitle>
+          <HeaderDesc>BTC: 41.9%</HeaderDesc>
+          <HeaderDesc>ETH: 19.1%</HeaderDesc>
+        </Col>
+        <Col className="header_darkmode">
+          <FaMoon />
         </Col>
       </Row>
     </Container>
--- src/Styles/MainHeaderStyle.ts
+++ src/Styles/MainHeaderStyle.ts
@@ -3,12 +3,15 @@ import "./font.scss";
 
 export const HeaderTitle = styled.p`
   margin-block-end: 0;
+  margin-inline-end: 0.5rem;
   color: var(--gray-9);
   font-size: 0.8rem;
+  font-weight: 600;
 `;
 
 export const HeaderDesc = styled.p`
   color: var(--blue-9);
   margin-block-end: 0;
   font-size: 0.8rem;
+  font-weight: 700;
 `;
