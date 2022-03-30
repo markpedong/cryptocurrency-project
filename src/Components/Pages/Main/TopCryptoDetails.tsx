f1dac3d495e7284621deab3913dadedf0c213b9e
markpedong <mark.pedong01@gmail.com>
2022-03-31 07:33:47 +0800

    Finished for the crypto heade

--- src/Components/Pages/Main/TopCryptoDetails.tsx
+++ src/Components/Pages/Main/TopCryptoDetails.tsx
@@ -1,9 +1,8 @@
-import { Col, Container, Row } from "react-bootstrap";
+import { Container, Row } from "react-bootstrap";
+import Slider from "react-slick";
+import TopCryptoDetailsData from "../../../Data/TopCryptoDetails.json";
 import "../../../Styles/TopCryptoDetails.scss";
 import { TopCryptoDetailsComponent } from "./TopCryptoDetailsComponent";
-import TopCryptoDetailsData from "../../../Data/TopCryptoDetails.json";
-import { useState } from "react";
-import Slider from "react-slick";
 
 export const TopCryptoDetails = () => {
   const settings = {
@@ -12,7 +11,42 @@ export const TopCryptoDetails = () => {
     speed: 500,
     slidesToShow: 5,
     slidesToScroll: 5,
+    nextArrow: <SampleNextArrow />,
+    prevArrow: <SamplePrevArrow />,
+    responsive: [
+      {
+        breakpoint: 1350,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 4,
+          infinite: false,
+          dots: false,
+        },
+      },
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
   };
+
+  function SampleNextArrow() {
+    return <div style={{ display: "none" }} />;
+  }
+
+  function SamplePrevArrow() {
+    return <div style={{ display: "none" }} />;
+  }
 
   return (
     <Container className="topcryptodetails_container">
